55e49395b50c73034b534446054f5b4d14b8c8ae
ale88andr <ale88andr@gmail.com>
2023-02-05 20:50:50 +0300

    add model replacements

--- breaks/admin.py
+++ breaks/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
+from django.contrib.admin import TabularInline
 
-from breaks.models import organisations
+from breaks.models import organisations, replacements
 
 
+####################
+# Inlines
+####################
+class ReplacementEmployeeInline(TabularInline):
+    model = replacements.ReplacementEmployee
+    fields = 'employee', 'status',
+
+
+####################
+# Models
+####################
 @admin.register(organisations.Organisation)
 class OrganisationAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'director',
@@ -11,3 +23,14 @@ class OrganisationAdmin(admin.ModelAdmin):
 @admin.register(organisations.Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'manager', 'min_active',
+
+
+@admin.register(replacements.Replacement)
+class ReplacementAdmin(admin.ModelAdmin):
+    list_display = 'id', 'group', 'date', 'break_start', 'break_end', 'break_max_duration',
+    inlines = ReplacementEmployeeInline,
+
+
+@admin.register(replacements.ReplacementStatus)
+class ReplacementStatusAdmin(admin.ModelAdmin):
+    list_display = 'code', 'name', 'sort', 'is_active',
