0c0d3237f82f3f10fd8f198591cdb84982b25aac
ale88andr <ale88andr@gmail.com>
2023-02-28 22:42:17 +0300

    feat: add registration point

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 from api.spectacular.urls import urlpatterns as doc_urlpatterns
+from users.urls import urlpatterns as user_urls
 
 
 app_name = 'api'
@@ -8,4 +9,6 @@ app_name = 'api'
 urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
 ]
+
 urlpatterns += doc_urlpatterns
+urlpatterns += user_urls
---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from users.views.users import RegistrationView
+
+urlpatterns = [
+    path('users/reg', RegistrationView.as_view(), name='reg')
+]
